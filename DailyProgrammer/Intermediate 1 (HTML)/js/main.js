7d81e901cc8afd0b021c256bbf6ad17c1321b152
Piotr Doan <doanpiotr@gmail.com>
2015-09-04 05:28:50 +0200

    DailyProgrammer - Intermediate 1: Create a function for adding events.

--- DailyProgrammer/Intermediate 1 (HTML)/js/main.js
+++ DailyProgrammer/Intermediate 1 (HTML)/js/main.js
@@ -1,19 +1,19 @@
 $("#addEventButton").click(function()
 {
-    // Show the modal dialog.
     $("#addEventModal").modal("show");
 });
 
 $("#addEventConfirmButton").click(function()
 {
-    // Add a new event entry.
-    var element = $("<tr>");
-    element.append($("<td>").text($("#inputDate").val()));
-    element.append($("<td>").text($("#inputTime").val()));
-    element.append($("<td>").text($("#inputDescription").val()));
-    element.append($("<td>").append($("<span>").addClass("glyphicon glyphicon-edit")));
+    // Get form input values.
+    var form = $("#addEventForm");
 
-    $("#eventList").append(element);
+    var date = form.find("#inputDate").val();
+    var time = form.find("#inputTime").val();
+    var description = form.find("#inputDescription").val();
+
+    // Add a new event.
+    addEvent(date, time, description);
 
     // Hide the modal dialog.
     $("#addEventModal").modal("hide");
@@ -21,3 +21,30 @@ $("#addEventConfirmButton").click(function()
     // Clear the dialog form.
     $("#addEventForm")[0].reset();
 });
+
+function addEvent(date, time, description)
+{
+    // Create an edit button.
+    var editButton = $("<span>");
+    editButton.attr("id", "editEventButton");
+    editButton.addClass("glyphicon glyphicon-edit");
+    editButton.click(function()
+    {
+        $("#editEventModal").modal("show");
+    });
+
+    // Create a list element.
+    var element = $("<tr>");
+    element.append($("<td>").text(date));
+    element.append($("<td>").text(time));
+    element.append($("<td>").text(description));
+    element.append($("<td>").append(editButton));
+
+    // Append element to the list.
+    $("#eventList").append(element);
+}
+
+// Add example events.
+addEvent("2015-09-01", "08:00", "Go out and have some fun at the city centre.");
+addEvent("2015-09-02", "14:30", "Drink some fine wine and get wasted.");
+addEvent("2015-09-03", "21:13", "Find the person you've been looking for your entire life.");
