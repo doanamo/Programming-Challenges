119de87807e2a2c21d1a345d2c11d308843625d7
Piotr Doan <doanpiotr@gmail.com>
2022-12-05 03:16:39 +0100

    Advent of Code 2022 Day 3 Simplify

--- AdventOfCode2022/src/day3.rs
+++ AdventOfCode2022/src/day3.rs
@@ -2,20 +2,19 @@ use itertools::Itertools;
 
 pub fn run()
 {
-    let input = std::fs::read_to_string("input/day3.txt").unwrap();
+    let input = include_str!("../input/day3.txt");
     let part1 = input.lines()
         .map(|line| line.split_at(line.chars().count() / 2))
         .map(|(left, right)| left.chars()
-            .filter(|char| right.find(*char).is_some())
+            .filter(|char| right.contains(*char))
             .next().unwrap() as u32);
     let part2 = input.lines().tuples::<(_, _, _)>()
         .map(|(first, second, third)| first.chars()
-            .filter(|char| second.find(*char).is_some())
-            .filter(|char| third.find(*char).is_some())
+            .filter(|char| second.contains(*char))
+            .filter(|char| third.contains(*char))
             .next().unwrap() as u32);
     let scoring = |score, char: u32|
-        if char > 96 { score + char - 96 }
-        else { score + char - 38 };
+        score + char - 38 - char / 96 * 58;
 
     println!("Day 3-1 answer: {}", part1.fold(0, scoring));
     println!("Day 3-2 answer: {}", part2.fold(0, scoring));
